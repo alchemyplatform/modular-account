3a34dcaf2f554b72c7d945b8c610275ac88fb9fa
adam <adam@alchemy.com>
2024-01-16 16:28:13 -0500

    Add ERC-20 spending limit selector check

--- src/plugins/session/permissions/SessionKeyPermissions.sol
+++ src/plugins/session/permissions/SessionKeyPermissions.sol
@@ -169,6 +169,11 @@ abstract contract SessionKeyPermissions is ISessionKeyPlugin, SessionKeyPermissi
             FunctionData storage functionData = _functionDataOf(msg.sender, keyId, target, selector);
             validationSuccess = !functionData.isOnList;
         }
+
+        // Check the selector in use if the target is a known ERC-20 contract with a spending limit.
+        if (contractData.isERC20WithSpendLimit && !isAllowedERC20Function(selector)) {
+            validationSuccess = false;
+        }
     }
 
     /// @dev Runs during execution to re-check and update the spend limits of the session key in use.
@@ -608,5 +613,9 @@ abstract contract SessionKeyPermissions is ISessionKeyPlugin, SessionKeyPermissi
 
     function _max(uint48 a, uint48 b) internal pure returns (uint48) {
         return a > b ? a : b;
+    }
+
+    function isAllowedERC20Function(bytes4 selector) internal pure returns (bool) {
+        return selector == IERC20.transfer.selector || selector == IERC20.approve.selector;
     }
 }
