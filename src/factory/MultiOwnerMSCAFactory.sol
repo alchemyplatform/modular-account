96d3429ff191bf2aaeab812be194e5e833cdde2a
howydev <132113803+howydev@users.noreply.github.com>
2024-01-17 12:42:24 -0500

    style: improve natspec comments

--- src/factory/MultiOwnerMSCAFactory.sol
+++ src/factory/MultiOwnerMSCAFactory.sol
@@ -116,7 +116,9 @@ contract MultiOwnerMSCAFactory is Ownable2Step {
 
     /// @notice Getter for counterfactual address based on input params
     /// @dev The owner array must be in strictly ascending order and not include the 0 address.
+    /// @param salt salt for additional entropy for create2
     /// @param owners array of addresses of the owner
+    /// @return address of counterfactual account
     function getAddress(uint256 salt, address[] calldata owners) external view returns (address) {
         // array can't be empty
         if (owners.length == 0) {
