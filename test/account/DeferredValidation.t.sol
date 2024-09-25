6cf8ca48902f5472a80cd9befe010b84db11f3c2
zer0dot <zer0dot.dev@gmail.com>
2024-09-25 18:10:39 -0400

    refactor: cleanup deferred validation test

--- test/account/DeferredValidation.t.sol
+++ test/account/DeferredValidation.t.sol
@@ -4,12 +4,11 @@ pragma solidity ^0.8.26;
 import {ModularAccount} from "../../src/account/ModularAccount.sol";
 
 import {IEntryPoint} from "@eth-infinitism/account-abstraction/interfaces/IEntryPoint.sol";
+import {PackedUserOperation} from "@eth-infinitism/account-abstraction/interfaces/PackedUserOperation.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 import {ModuleEntity, ModuleEntityLib} from "../../src/libraries/ModuleEntityLib.sol";
 import {ValidationConfig, ValidationConfigLib} from "../../src/libraries/ValidationConfigLib.sol";
-
-import {MockUserOpValidationModule} from "../mocks/modules/ValidationModuleMocks.sol";
-import {PackedUserOperation} from "@eth-infinitism/account-abstraction/interfaces/PackedUserOperation.sol";
 
 import {AccountTestBase} from "../utils/AccountTestBase.sol";
 
@@ -21,39 +20,55 @@ contract DeferredValidationTest is AccountTestBase {
         "InstallValidation(bytes25 validationConfig,bytes4[] selectors,bytes installData,bytes[] hooks,uint256 nonce,uint48 deadline)"
     );
 
-    bytes internal _encodedCall = abi.encodeCall(ModularAccount.execute, (makeAddr("dead"), 0, ""));
-    ModuleEntity internal _mockValidation;
+    bytes internal _encodedCall;
+    ModuleEntity internal _deferredValidation;
     bool internal _isSmaTest;
-    bytes internal _deferredValidationSig;
     bytes internal _deferredValidationInstallData;
 
     function setUp() external {
-        _mockValidation = ModuleEntityLib.pack(address(new MockUserOpValidationModule()), 0); // todo consider
-            // return data
+        _encodedCall = abi.encodeCall(ModularAccount.execute, (makeAddr("dead"), 0, ""));
+        _deferredValidation = ModuleEntityLib.pack(address(_deploySingleSignerValidationModule()), 0);
         _isSmaTest = vm.envOr("SMA_TEST", false);
-        _deferredValidationInstallData = "";
-        _deferredValidationSig = "";
+
+        uint32 entityId = 0;
+        _deferredValidationInstallData = abi.encode(entityId, owner1);
     }
 
     // Negatives
 
     function test_fail_deferredValidation_nonceUsed() external {
-        _runUserOpWithCustomSig(
-            _encodedCall,
-            "",
-            _buildFullDeferredInstallSig(
-                vm,
-                owner1Key,
-                _isSmaTest,
-                account1,
-                _signerValidation,
-                _mockValidation,
-                _deferredValidationInstallData,
-                _deferredValidationSig,
-                0,
-                0
-            )
+        uint256 nonce = entryPoint.getNonce(address(account1), 0);
+
+        PackedUserOperation memory userOp = PackedUserOperation({
+            sender: address(account1),
+            nonce: nonce,
+            initCode: hex"",
+            callData: _encodedCall,
+            accountGasLimits: _encodeGas(VERIFICATION_GAS_LIMIT, CALL_GAS_LIMIT),
+            preVerificationGas: 0,
+            gasFees: _encodeGas(1, 1),
+            paymasterAndData: hex"",
+            signature: ""
+        });
+
+        bytes32 userOpHash = entryPoint.getUserOpHash(userOp);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(owner1Key, MessageHashUtils.toEthSignedMessageHash(userOpHash));
+        bytes memory deferredValidationSig = abi.encodePacked(r, s, v);
+
+        userOp.signature = _buildFullDeferredInstallSig(
+            vm,
+            owner1Key,
+            _isSmaTest,
+            account1,
+            _signerValidation,
+            _deferredValidation,
+            _deferredValidationInstallData,
+            deferredValidationSig,
+            0,
+            0
         );
+
+        _sendOp(userOp, "");
 
         bytes memory expectedRevertdata = abi.encodeWithSelector(
             IEntryPoint.FailedOpWithRevert.selector,
@@ -62,76 +77,126 @@ contract DeferredValidationTest is AccountTestBase {
             abi.encodeWithSelector(ModularAccount.DeferredInstallNonceInvalid.selector)
         );
 
-        _runUserOpWithCustomSig(
-            _encodedCall,
-            expectedRevertdata,
-            _buildFullDeferredInstallSig(
-                vm,
-                owner1Key,
-                _isSmaTest,
-                account1,
-                _signerValidation,
-                _mockValidation,
-                _deferredValidationInstallData,
-                _deferredValidationSig,
-                0,
-                0
-            )
-        );
+        _sendOp(userOp, expectedRevertdata);
     }
 
     function test_fail_deferredValidation_pastDeadline() external {
+        // Note that a deadline of 0 implies no expiry
+        vm.warp(2);
+
+        uint256 nonce = entryPoint.getNonce(address(account1), 0);
+
+        PackedUserOperation memory userOp = PackedUserOperation({
+            sender: address(account1),
+            nonce: nonce,
+            initCode: hex"",
+            callData: _encodedCall,
+            accountGasLimits: _encodeGas(VERIFICATION_GAS_LIMIT, CALL_GAS_LIMIT),
+            preVerificationGas: 0,
+            gasFees: _encodeGas(1, 1),
+            paymasterAndData: hex"",
+            signature: ""
+        });
+
+        bytes32 userOpHash = entryPoint.getUserOpHash(userOp);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(owner1Key, MessageHashUtils.toEthSignedMessageHash(userOpHash));
+        bytes memory deferredValidationSig = abi.encodePacked(r, s, v);
+
+        userOp.signature = _buildFullDeferredInstallSig(
+            vm,
+            owner1Key,
+            _isSmaTest,
+            account1,
+            _signerValidation,
+            _deferredValidation,
+            _deferredValidationInstallData,
+            deferredValidationSig,
+            0,
+            1
+        );
+
         bytes memory expectedRevertdata =
             abi.encodeWithSelector(IEntryPoint.FailedOp.selector, 0, "AA22 expired or not due");
 
-        // Note that a deadline of 0 implies no expiry
-        vm.warp(2);
-        _runUserOpWithCustomSig(
-            _encodedCall,
-            expectedRevertdata,
-            _buildFullDeferredInstallSig(
-                vm,
-                owner1Key,
-                _isSmaTest,
-                account1,
-                _signerValidation,
-                _mockValidation,
-                _deferredValidationInstallData,
-                _deferredValidationSig,
-                0,
-                1
-            )
-        );
+        _sendOp(userOp, expectedRevertdata);
     }
 
     function test_fail_deferredValidation_invalidSig() external {
+        uint256 nonce = entryPoint.getNonce(address(account1), 0);
+
+        PackedUserOperation memory userOp = PackedUserOperation({
+            sender: address(account1),
+            nonce: nonce,
+            initCode: hex"",
+            callData: _encodedCall,
+            accountGasLimits: _encodeGas(VERIFICATION_GAS_LIMIT, CALL_GAS_LIMIT),
+            preVerificationGas: 0,
+            gasFees: _encodeGas(1, 1),
+            paymasterAndData: hex"",
+            signature: ""
+        });
+
+        bytes32 userOpHash = entryPoint.getUserOpHash(userOp);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(owner1Key, MessageHashUtils.toEthSignedMessageHash(userOpHash));
+        bytes memory deferredValidationSig = abi.encodePacked(r, s, v);
+
+        userOp.signature = _buildFullDeferredInstallSig(
+            vm,
+            owner1Key,
+            _isSmaTest,
+            ModularAccount(payable(0)),
+            _signerValidation,
+            _deferredValidation,
+            _deferredValidationInstallData,
+            deferredValidationSig,
+            0,
+            0
+        );
+
         bytes memory expectedRevertData = abi.encodeWithSelector(
             IEntryPoint.FailedOpWithRevert.selector,
             0,
             "AA23 reverted",
             abi.encodeWithSelector(ModularAccount.DeferredInstallSignatureInvalid.selector)
         );
-        _runUserOpWithCustomSig(
-            _encodedCall,
-            expectedRevertData,
-            _buildFullDeferredInstallSig(
-                vm,
-                owner1Key,
-                _isSmaTest,
-                ModularAccount(payable(0)),
-                _signerValidation,
-                _mockValidation,
-                _deferredValidationInstallData,
-                _deferredValidationSig,
-                0,
-                0
-            )
-        );
+
+        _sendOp(userOp, expectedRevertData);
     }
 
     function test_fail_deferredValidation_nonceInvalidated() external {
         vm.prank(address(entryPoint));
         account1.invalidateDeferredValidationInstallNonce(0);
+
+        uint256 nonce = entryPoint.getNonce(address(account1), 0);
+
+        PackedUserOperation memory userOp = PackedUserOperation({
+            sender: address(account1),
+            nonce: nonce,
+            initCode: hex"",
+            callData: _encodedCall,
+            accountGasLimits: _encodeGas(VERIFICATION_GAS_LIMIT, CALL_GAS_LIMIT),
+            preVerificationGas: 0,
+            gasFees: _encodeGas(1, 1),
+            paymasterAndData: hex"",
+            signature: ""
+        });
+
+        bytes32 userOpHash = entryPoint.getUserOpHash(userOp);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(owner1Key, MessageHashUtils.toEthSignedMessageHash(userOpHash));
+        bytes memory deferredValidationSig = abi.encodePacked(r, s, v);
+
+        userOp.signature = _buildFullDeferredInstallSig(
+            vm,
+            owner1Key,
+            _isSmaTest,
+            account1,
+            _signerValidation,
+            _deferredValidation,
+            _deferredValidationInstallData,
+            deferredValidationSig,
+            0,
+            0
+        );
 
         bytes memory expectedRevertdata = abi.encodeWithSelector(
             IEntryPoint.FailedOpWithRevert.selector,
@@ -140,44 +205,44 @@ contract DeferredValidationTest is AccountTestBase {
             abi.encodeWithSelector(ModularAccount.DeferredInstallNonceInvalid.selector)
         );
 
-        _runUserOpWithCustomSig(
-            _encodedCall,
-            expectedRevertdata,
-            _buildFullDeferredInstallSig(
-                vm,
-                owner1Key,
-                _isSmaTest,
-                account1,
-                _signerValidation,
-                _mockValidation,
-                _deferredValidationInstallData,
-                _deferredValidationSig,
-                0,
-                0
-            )
-        );
+        _sendOp(userOp, expectedRevertdata);
     }
 
-    // TODO: Test with hooks
     // Positives
 
     function test_deferredValidation() external {
-        _runUserOpWithCustomSig(
-            _encodedCall,
-            "",
-            _buildFullDeferredInstallSig(
-                vm,
-                owner1Key,
-                _isSmaTest,
-                account1,
-                _signerValidation,
-                _mockValidation,
-                _deferredValidationInstallData,
-                _deferredValidationSig,
-                0,
-                0
-            )
+        uint256 nonce = entryPoint.getNonce(address(account1), 0);
+
+        PackedUserOperation memory userOp = PackedUserOperation({
+            sender: address(account1),
+            nonce: nonce,
+            initCode: hex"",
+            callData: _encodedCall,
+            accountGasLimits: _encodeGas(VERIFICATION_GAS_LIMIT, CALL_GAS_LIMIT),
+            preVerificationGas: 0,
+            gasFees: _encodeGas(1, 1),
+            paymasterAndData: hex"",
+            signature: ""
+        });
+
+        bytes32 userOpHash = entryPoint.getUserOpHash(userOp);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(owner1Key, MessageHashUtils.toEthSignedMessageHash(userOpHash));
+        bytes memory deferredValidationSig = abi.encodePacked(r, s, v);
+
+        userOp.signature = _buildFullDeferredInstallSig(
+            vm,
+            owner1Key,
+            _isSmaTest,
+            account1,
+            _signerValidation,
+            _deferredValidation,
+            _deferredValidationInstallData,
+            deferredValidationSig,
+            0,
+            0
         );
+
+        _sendOp(userOp, "");
     }
 
     function test_deferredValidation_initCode() external {
@@ -199,50 +264,38 @@ contract DeferredValidationTest is AccountTestBase {
         // prefund
         vm.deal(address(account2), 100 ether);
 
+        uint256 nonce = entryPoint.getNonce(address(account1), 0);
+
         PackedUserOperation memory userOp = PackedUserOperation({
             sender: address(account2),
-            nonce: 0,
+            nonce: nonce,
             initCode: initCode,
             callData: _encodedCall,
             accountGasLimits: _encodeGas(VERIFICATION_GAS_LIMIT, CALL_GAS_LIMIT),
             preVerificationGas: 0,
-            gasFees: _encodeGas(1, 2),
-            paymasterAndData: "",
-            signature: _buildFullDeferredInstallSig(
-                vm,
-                owner1Key,
-                _isSmaTest,
-                account2,
-                _signerValidation,
-                _mockValidation,
-                _deferredValidationInstallData,
-                _deferredValidationSig,
-                0,
-                0
-            )
-        });
-
-        _sendOp(userOp, "");
-    }
-
-    function _runUserOpWithCustomSig(bytes memory callData, bytes memory expectedRevertData, bytes memory sig)
-        internal
-    {
-        uint256 nonce = entryPoint.getNonce(address(account1), 0);
-
-        PackedUserOperation memory userOp = PackedUserOperation({
-            sender: address(account1),
-            nonce: nonce,
-            initCode: hex"",
-            callData: callData,
-            accountGasLimits: _encodeGas(VERIFICATION_GAS_LIMIT, CALL_GAS_LIMIT),
-            preVerificationGas: 0,
             gasFees: _encodeGas(1, 1),
             paymasterAndData: hex"",
-            signature: sig
+            signature: ""
         });
 
-        _sendOp(userOp, expectedRevertData);
+        bytes32 userOpHash = entryPoint.getUserOpHash(userOp);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(owner1Key, MessageHashUtils.toEthSignedMessageHash(userOpHash));
+        bytes memory deferredValidationSig = abi.encodePacked(r, s, v);
+
+        userOp.signature = _buildFullDeferredInstallSig(
+            vm,
+            owner1Key,
+            _isSmaTest,
+            account2,
+            _signerValidation,
+            _deferredValidation,
+            _deferredValidationInstallData,
+            deferredValidationSig,
+            0,
+            0
+        );
+
+        _sendOp(userOp, "");
     }
 
     function _sendOp(PackedUserOperation memory userOp, bytes memory expectedRevertData) internal {
