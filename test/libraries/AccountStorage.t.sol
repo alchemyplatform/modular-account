7e8b7038a0c8b736027cfc23300de3de26ebd8d1
Adam Egyed <5456061+adamegyed@users.noreply.github.com>
2024-10-02 18:16:11 -0400

    test: port library tests and add type safety (#208)
    Co-authored-by: fangting-alchemy <119372438+fangting-alchemy@users.noreply.github.com>

---
+++ test/libraries/AccountStorage.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.26;
+
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Test} from "forge-std/src/Test.sol";
+
+import {_ACCOUNT_STORAGE_SLOT} from "../../src/account/AccountStorage.sol";
+import {AccountStorageInitializable} from "../../src/account/AccountStorageInitializable.sol";
+import {MockDiamondStorageContract} from "../mocks/MockDiamondStorageContract.sol";
+
+// Test implementation of AccountStorageInitializable which is contained in ReferenceModularAccount
+contract AccountStorageTest is Test {
+    MockDiamondStorageContract public impl;
+    address public proxy;
+
+    function setUp() external {
+        impl = new MockDiamondStorageContract();
+        proxy = address(new ERC1967Proxy(address(impl), ""));
+    }
+
+    function test_storageSlotImpl() external {
+        // disable initializers sets value to uint8(max)
+        assertEq(uint256(vm.load(address(impl), _ACCOUNT_STORAGE_SLOT)), type(uint8).max);
+
+        // should revert if we try to initialize again
+        vm.expectRevert(AccountStorageInitializable.InvalidInitialization.selector);
+        impl.initialize();
+    }
+
+    function test_storageSlotProxy() external {
+        // before init, proxy's slot should be empty
+        assertEq(uint256(vm.load(proxy, _ACCOUNT_STORAGE_SLOT)), uint256(0));
+
+        MockDiamondStorageContract(proxy).initialize();
+        // post init slot should contains: packed(uint8 initialized = 1, bool initializing = 0)
+        assertEq(uint256(vm.load(proxy, _ACCOUNT_STORAGE_SLOT)), uint256(1));
+    }
+}
---
+++ test/libraries/SparseCalldataSegmentLib.t.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.26;
+
+import {Test} from "forge-std/src/Test.sol";
+
+import {SparseCalldataSegmentLib} from "../../src/libraries/SparseCalldataSegmentLib.sol";
+
+contract SparseCalldataSegmentLibTest is Test {
+    using SparseCalldataSegmentLib for bytes;
+
+    function testFuzz_sparseCalldataSegmentLib_encodeDecode_simple(bytes[] memory segments) public view {
+        bytes memory encoded = _encodeSimple(segments);
+        bytes[] memory decoded = this.decodeSimple(encoded, segments.length);
+
+        assertEq(decoded.length, segments.length, "decoded.length != segments.length");
+
+        for (uint256 i = 0; i < segments.length; i++) {
+            assertEq(decoded[i], segments[i]);
+        }
+    }
+
+    function testFuzz_sparseCalldataSegmentLib_encodeDecode_withIndex(bytes[] memory segments, uint256 indexSeed)
+        public
+        view
+    {
+        // Generate random indices
+        uint8[] memory indices = new uint8[](segments.length);
+        for (uint256 i = 0; i < segments.length; i++) {
+            uint8 nextIndex = uint8(uint256(keccak256(abi.encodePacked(indexSeed, i))));
+            indices[i] = nextIndex;
+        }
+
+        // Encode
+        bytes memory encoded = _encodeWithIndex(segments, indices);
+
+        // Decode
+        (bytes[] memory decodedBodies, uint8[] memory decodedIndices) =
+            this.decodeWithIndex(encoded, segments.length);
+
+        assertEq(decodedBodies.length, segments.length, "decodedBodies.length != segments.length");
+        assertEq(decodedIndices.length, segments.length, "decodedIndices.length != segments.length");
+
+        for (uint256 i = 0; i < segments.length; i++) {
+            assertEq(decodedBodies[i], segments[i]);
+            assertEq(decodedIndices[i], indices[i]);
+        }
+    }
+
+    function _encodeSimple(bytes[] memory segments) internal pure returns (bytes memory) {
+        bytes memory result = "";
+
+        for (uint256 i = 0; i < segments.length; i++) {
+            result = abi.encodePacked(result, uint32(segments[i].length), segments[i]);
+        }
+
+        return result;
+    }
+
+    function _encodeWithIndex(bytes[] memory segments, uint8[] memory indices)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        require(segments.length == indices.length, "segments len != indices len");
+
+        bytes memory result = "";
+
+        for (uint256 i = 0; i < segments.length; i++) {
+            result = abi.encodePacked(result, uint32(segments[i].length + 1), indices[i], segments[i]);
+        }
+
+        return result;
+    }
+
+    function decodeSimple(bytes calldata encoded, uint256 capacityHint) external pure returns (bytes[] memory) {
+        bytes[] memory result = new bytes[](capacityHint);
+
+        bytes calldata remainder = encoded;
+
+        uint256 index = 0;
+        while (remainder.length > 0) {
+            bytes calldata segment;
+            (segment, remainder) = remainder.getNextSegment();
+            result[index] = segment;
+            index++;
+        }
+
+        return result;
+    }
+
+    function decodeWithIndex(bytes calldata encoded, uint256 capacityHint)
+        external
+        pure
+        returns (bytes[] memory, uint8[] memory)
+    {
+        bytes[] memory bodies = new bytes[](capacityHint);
+        uint8[] memory indices = new uint8[](capacityHint);
+
+        bytes calldata remainder = encoded;
+
+        uint256 index = 0;
+        while (remainder.length > 0) {
+            bytes calldata segment;
+            (segment, remainder) = remainder.getNextSegment();
+            bodies[index] = segment.getBody();
+            indices[index] = segment.getIndex();
+            index++;
+        }
+
+        return (bodies, indices);
+    }
+}
