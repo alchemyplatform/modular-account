1666bcfd1bef6dcaf89372b17faafdc7ea1f9f67
fangting-alchemy <119372438+fangting-alchemy@users.noreply.github.com>
2024-10-04 10:26:10 -0700

    feat: add factory token sweeping (#221)

--- test/factory/AccountFactory.t.sol
+++ test/factory/AccountFactory.t.sol
@@ -6,7 +6,11 @@ import {ModularAccount} from "../../src/account/ModularAccount.sol";
 import {AccountTestBase} from "../utils/AccountTestBase.sol";
 import {TEST_DEFAULT_VALIDATION_ENTITY_ID} from "../utils/TestConstants.sol";
 
+import {MockERC20} from "../mocks/MockERC20.sol";
+
 contract AccountFactoryTest is AccountTestBase {
+    MockERC20 public erc20;
+
     function test_createAccount() public withSMATest {
         ModularAccount account = factory.createAccount(address(this), 100, TEST_DEFAULT_VALIDATION_ENTITY_ID);
 
@@ -34,5 +38,15 @@ contract AccountFactoryTest is AccountTestBase {
 
         // Assert the return addresses are the same
         assertEq(address(account), address(account2));
+    }
+
+    function test_withdraw() public {
+        erc20 = new MockERC20();
+        erc20.mint(address(factory), 10 ether);
+
+        assertEq(erc20.balanceOf(address(factory)), 10 ether);
+        vm.prank(factoryOwner);
+        factory.withdraw(payable(address(this)), address(erc20), 10 ether); // amount = balance if native currency
+        assertEq(address(factory).balance, 0);
     }
 }
