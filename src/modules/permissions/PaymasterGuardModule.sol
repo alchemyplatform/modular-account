cb5f3134a8f67ee6dd609c2458135baa798a8ec5
Fangting Liu <fangting@alchemy.com>
2024-10-14 13:33:02 -0700

    update modules license

--- src/modules/permissions/PaymasterGuardModule.sol
+++ src/modules/permissions/PaymasterGuardModule.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.26;
 
 import {PackedUserOperation} from "@eth-infinitism/account-abstraction/interfaces/PackedUserOperation.sol";
--- src/modules/permissions/TimeRangeModule.sol
+++ src/modules/permissions/TimeRangeModule.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.26;
 
 import {_packValidationData} from "@eth-infinitism/account-abstraction/core/Helpers.sol";
