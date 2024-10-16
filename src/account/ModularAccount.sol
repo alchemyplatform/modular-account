88cd733824fb83cff63560c5d7147024dae7093b
Jay Paik <jaypaik@users.noreply.github.com>
2024-10-16 18:21:23 -0400

    chore: pragma and licenses cleanup (#262)

--- src/account/ModularAccount.sol
+++ src/account/ModularAccount.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.26;
+pragma solidity ^0.8.26;
 
 import {ValidationConfig} from "@erc6900/reference-implementation/interfaces/IModularAccount.sol";
 import {IEntryPoint} from "@eth-infinitism/account-abstraction/interfaces/IEntryPoint.sol";
