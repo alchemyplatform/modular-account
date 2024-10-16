bbc4c4509e0537a3e03dd82f97b696514dd06c01
Jay Paik <jay@alchemy.com>
2024-10-16 19:51:04 -0400

    chore: KnownSelectorsLib natspec

--- src/libraries/KnownSelectorsLib.sol
+++ src/libraries/KnownSelectorsLib.sol
@@ -9,9 +9,14 @@ import {IValidationModule} from "@erc6900/reference-implementation/interfaces/IV
 import {IAggregator} from "@eth-infinitism/account-abstraction/interfaces/IAggregator.sol";
 import {IPaymaster} from "@eth-infinitism/account-abstraction/interfaces/IPaymaster.sol";
 
-/// @dev Library to help to check if a selector is a know function selector of the modular account or ERC-4337
-/// contract.
+/// @title Known Selectors Library
+/// @author Alchemy
+/// @notice Library to help to check if a selector is an ERC-6900 module function or a an ERC-4337 contract
+/// function.
 library KnownSelectorsLib {
+    /// @notice Check if a selector is an ERC-4337 function.
+    /// @param selector The selector to check.
+    /// @return True if the selector is an ERC-4337 function, false otherwise.
     function isErc4337Function(uint32 selector) internal pure returns (bool) {
         return selector == uint32(IAggregator.validateSignatures.selector)
             || selector == uint32(IAggregator.validateUserOpSignature.selector)
@@ -20,6 +25,9 @@ library KnownSelectorsLib {
             || selector == uint32(IPaymaster.postOp.selector);
     }
 
+    /// @notice Check if a selector is an ERC-6900 module function.
+    /// @param selector The selector to check.
+    /// @return True if the selector is an ERC-6900 module function, false otherwise.
     function isIModuleFunction(uint32 selector) internal pure returns (bool) {
         return selector == uint32(IModule.onInstall.selector) || selector == uint32(IModule.onUninstall.selector)
             || selector == uint32(IModule.moduleId.selector)
