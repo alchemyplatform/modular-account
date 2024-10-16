fc33d2dc3d3ad0bf50b6065e8e702b29549e4a80
Fangting Liu <fangting@alchemy.com>
2024-10-15 19:01:59 -0700

    fix edge case and add tests

--- test/helpers/ValidationResHelpers.t.sol
+++ test/helpers/ValidationResHelpers.t.sol
@@ -3,16 +3,52 @@ pragma solidity ^0.8.26;
 
 import {Test} from "forge-std/src/Test.sol";
 
-import {_packValidationData} from "@eth-infinitism/account-abstraction/core/Helpers.sol";
+import {
+    ValidationData,
+    _packValidationData,
+    _packValidationData
+} from "@eth-infinitism/account-abstraction/core/Helpers.sol";
 
 import {_coalesceValidationRes} from "../../src/helpers/ValidationResHelpers.sol";
 
 contract ValidationResHelpersTest is Test {
     function test_coalesceValidationRes() public pure {
+        // both validation res have all three values, one return aggregator address, one success
+        assertEq(
+            _coalesceValidationRes(
+                _packValidationData(false, uint48(5), uint48(1)),
+                _packValidationData(ValidationData(address(2), uint48(2), uint48(6)))
+            ),
+            _packValidationData(ValidationData(address(2), uint48(2), uint48(5)))
+        );
+        assertEq(
+            _coalesceValidationRes(
+                _packValidationData(ValidationData(address(2), uint48(2), uint48(6))),
+                _packValidationData(false, uint48(5), uint48(1))
+            ),
+            _packValidationData(ValidationData(address(2), uint48(2), uint48(5)))
+        );
+
+        // both validation res have all three values, one return aggregator address, one fail
+        assertEq(
+            _coalesceValidationRes(
+                _packValidationData(true, uint48(5), uint48(1)),
+                _packValidationData(ValidationData(address(2), uint48(2), uint48(6)))
+            ),
+            _packValidationData(true, uint48(5), uint48(2))
+        );
+        assertEq(
+            _coalesceValidationRes(
+                _packValidationData(ValidationData(address(2), uint48(2), uint48(6))),
+                _packValidationData(true, uint48(5), uint48(1))
+            ),
+            _packValidationData(true, uint48(5), uint48(2))
+        );
+
         // both validation res have all three values
         assertEq(
             _coalesceValidationRes(
-                _packValidationData(false, uint48(5), uint48(2)), _packValidationData(false, uint48(6), uint48(2))
+                _packValidationData(false, uint48(5), uint48(1)), _packValidationData(false, uint48(6), uint48(2))
             ),
             _packValidationData(false, uint48(5), uint48(2))
         );
